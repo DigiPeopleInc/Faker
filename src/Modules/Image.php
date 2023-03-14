7ad2bab19a70b975572e9726d524307dd0a8a740
EgorBurdel <IgnessBMusic@gmail.com>
2023-03-14 16:24:54 +0500

    directory separator fix

--- src/Modules/Image.php
+++ src/Modules/Image.php
@@ -26,8 +26,11 @@ class Image extends AbstractModule
             throw new Exception("Размер не может быть меньше 1");
         }
         if ($width > Config::LOREMFLICKR_PICTURE_MAX_WIDTH || $height > Config::LOREMFLICKR_PICTURE_MAX_HEIGHT) {
-            throw new Exception("Размер изображения должен быть не более ".Config::LOREMFLICKR_PICTURE_MAX_WIDTH."x".Config::LOREMFLICKR_PICTURE_MAX_HEIGHT);
+            throw new Exception(
+                "Размер изображения должен быть не более ".Config::LOREMFLICKR_PICTURE_MAX_WIDTH."x".Config::LOREMFLICKR_PICTURE_MAX_HEIGHT
+            );
         }
+
         return Helpers\ExternalSource::get($url);
     }
 
@@ -40,8 +43,14 @@ class Image extends AbstractModule
      * @return string
      * @throws Exception
      */
-    public function image(int $width = 512, int $height = 512, string $backgroundColor = "808080", string $extension = "jpg", string $text = "", string $textColor = "000000"): string
-    {
+    public function image(
+        int $width = 512,
+        int $height = 512,
+        string $backgroundColor = "808080",
+        string $extension = "jpg",
+        string $text = "",
+        string $textColor = "000000"
+    ): string {
         $backgroundColor = str_replace("#", "", $backgroundColor);
         $textColor = str_replace("#", "", $textColor);
         $allowedExtensions = [
@@ -50,7 +59,7 @@ class Image extends AbstractModule
             "jpg",
             "jpeg",
             "png",
-            "webp"
+            "webp",
         ];
         $r = (int)hexdec(substr($backgroundColor, 0, 2));
         $g = (int)hexdec(substr($backgroundColor, 2, 2));
@@ -59,10 +68,14 @@ class Image extends AbstractModule
             throw new Exception("Размер не может быть меньше 1");
         }
         if ($width > Config::PICTURE_MAX_WIDTH || $height > Config::PICTURE_MAX_HEIGHT) {
-            throw new Exception("Размер изображения должен быть не более ".Config::PICTURE_MAX_WIDTH."x".Config::PICTURE_MAX_HEIGHT);
+            throw new Exception(
+                "Размер изображения должен быть не более ".Config::PICTURE_MAX_WIDTH."x".Config::PICTURE_MAX_HEIGHT
+            );
         }
         if (!in_array($extension, $allowedExtensions)) {
-            throw new Exception("Расширение изображения не поддерживается, доступные расширения: ".implode(", ", $allowedExtensions));
+            throw new Exception(
+                "Расширение изображения не поддерживается, доступные расширения: ".implode(", ", $allowedExtensions)
+            );
         }
         $image = imageCreate($width, $height);
         $background = imageColorAllocate($image, $r, $g, $b);
@@ -73,40 +86,47 @@ class Image extends AbstractModule
             $gText = (int)hexdec(substr($textColor, 2, 2));
             $bText = (int)hexdec(substr($textColor, 4, 2));
             $foreground = imageColorAllocate($image, $rText, $gText, $bText);
-            $fontSize = max( min( $width / mb_strlen($text) * 1.15, $height * 0.5 ), 5 );
+            $fontSize = max(min($width / mb_strlen($text) * 1.15, $height * 0.5), 5);
             $textBox = Helpers\Image::TextBox($fontSize, $font, $text);
-            $textWidth = ceil( ($textBox[4] - $textBox[1] ) * 1.07);
-            $textHeight = ceil( (abs( $textBox[7] ) + abs( $textBox[1] ) ) * 1);
-            $textX = ceil( ( $width - $textWidth ) / 2 );
-            $textY = ceil( ( $height - $textHeight ) / 2 + $textHeight );
+            $textWidth = ceil(($textBox[4] - $textBox[1]) * 1.07);
+            $textHeight = ceil((abs($textBox[7]) + abs($textBox[1])) * 1);
+            $textX = ceil(($width - $textWidth) / 2);
+            $textY = ceil(($height - $textHeight) / 2 + $textHeight);
             imagettftext($image, $fontSize, 0, $textX, $textY, $foreground, $font, $text);
         }
         ob_start();
         switch ($extension) {
-            case 'bmp': {
+            case 'bmp':
+            {
                 imagebmp($image);
                 break;
             }
-            case 'gif': {
+            case 'gif':
+            {
                 imagegif($image);
-                break;            }
+                break;
+            }
 
-            case 'png': {
+            case 'png':
+            {
                 imagepng($image);
                 break;
             }
             case 'jpg':
-            case 'jpeg': {
+            case 'jpeg':
+            {
                 imagejpeg($image);
                 break;
             }
-            case 'webp': {
+            case 'webp':
+            {
                 imagewebp($image);
                 break;
             }
         }
         $result = ob_get_contents();
         ob_end_clean();
+
         return $result;
     }
 }
